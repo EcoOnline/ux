1fa7a7c12515d3ac844263a75d8bbe76a0c3de8d
Hayden <haydenchambers@gmail.com>
2021-08-31 09:17:42 +0300

    product montly

--- src/What_is_it.svelte
+++ src/What_is_it.svelte
@@ -23,7 +23,7 @@
 <div class="page">
 	<h1>EcoOnline UX Tools & Experiments</h1>
 
-	{#if step === 0}
+	{#if step < 2}
 		<h4>Who am I</h4>
 		<ul>
 			<li><b>Hayden Chambers</b></li>
@@ -32,17 +32,19 @@
 			<li>- UX/UI</li>
 			<li>- frontend JS coding</li>
 		</ul>
-		<h4>What do I do now?</h4>
-		<ul>
-			<li>Started with the rebrand</li>
-			<li>Assist with Design System components</li>
-			<li>Create icons and ux mockups</li>
-			<li>Make Arnfinn lose more hair</li>
-			<li>And...</li>
-			<li><b>Tools and experiments</b></li>
-		</ul>
+		{#if step > 0}
+			<h4>What do I do now?</h4>
+			<ul>
+				<li>Started with the rebrand</li>
+				<li>Assist with Design System components</li>
+				<li>Create icons and ux mockups</li>
+				<li>Make Arnfinn lose more hair</li>
+				<li>And...</li>
+				<li><b>Tools and experiments</b></li>
+			</ul>
+		{/if}
 	{/if}
-	{#if step > 0}
+	{#if step > 1}
 		<h4>What are tools and experiments?</h4>
 		<ul>
 			<li><b>EXPERIMENTS</b></li>

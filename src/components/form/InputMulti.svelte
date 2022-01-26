961d67b086ea1024ed08bdbf30e98dfa460236b9
Hayden <haydenchambers@gmail.com>
2022-01-26 13:31:51 +0100

    training update to include Praxis 42

--- src/components/form/InputMulti.svelte
+++ src/components/form/InputMulti.svelte
@@ -1,16 +1,49 @@
 <script>
+    import { onMount} from 'svelte';
     export let f;
+
+    let item = false;
+    let selected = [];
+    let selected_shortlist = [];
+
+    function tree_selected(arr) {
+        let temp_selected = []
+        arr.forEach( (option) => {
+
+            if(option.selected) {
+                temp_selected.push(option)
+            }
+            if(option.children && option.children.length){
+                temp_selected = [...temp_selected, ...tree_selected(option.children)]
+            }
+        })
+        return temp_selected;
+    }
+    $: {
+        selected = tree_selected(f.options);
+        console.log('selected!!', selected);
+    }
+
+    onMount(() => {
+		console.log("item", item.offsetWidth);
+
+        
+	})
 </script>
 
 
-<div class="form-item">
+<div class="form-item" bind:this="{item}">
     {#if f.label}
         <label for="{f.id}">{f.label}</label>
     {/if}
     {#if f.hint}
         <p>{f.hint}</p>
     {/if}
-
+    {#if selected.length}
+        {#each selected_shortlist as tag}
+            tag
+        {/each}
+    {/if}
     <input id="{f.id}" bind:value="{f.answer}" type="text" placeholder="{f.placeholder ? f.placeholder : ''}" class="form-control">
     multiiii
 </div>

7ed64b8a0c61bd89efe292b52ff8313b8f4e1938
Hayden <haydenchambers@gmail.com>
2022-07-18 12:54:45 +0200

    sidebar

--- src/Frame_platform.svelte
+++ src/Frame_platform.svelte
@@ -1,13 +1,36 @@
 <script>
-    import { onMount } from 'svelte';
-    /*
-    onMount( ()=> {
-        console.log('Platform not ready, re-directing to EHS');
-        window.location.hash = '#ehs'; 
-    })
-    */
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    function nav(str) {
+		dispatch('nav', {
+			text: str
+		});
+	}
+    let tab = "apps";
+    export let tabnav = '';
+    $: {
+        let t = tabnav;
+        tab = (t == '' ? 'apps' : t);
+    }
 
 </script>
 
-<h2>Platform...</h2>
+<h2>EcoOnline Home</h2>
+
+<ul class="tabs">
+    <li><a href="#platform" class:active="{tab == 'apps'}">Applications</a></li>
+    <li><a href="#platform/news" class:active="{tab == 'news'}">News</a></li>
+    <li><a href="#platform/dashboards" class:active="{tab == 'dashboards'}">Dashboards</a></li>
+</ul>
+
+
+{#if tab == 'apps'}
+    <div class="row">
+        <div class="col3 col-sm-2">
+            sidebar
+        </div>
+    </div>
+{/if}
+
         

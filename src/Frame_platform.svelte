05d70ba06bc61243a4e720e7eb8811e5652a72e9
Hayden <haydenchambers@gmail.com>
2022-07-18 12:45:27 +0200

    platform test

--- src/Frame_platform.svelte
+++ src/Frame_platform.svelte
@@ -1,10 +1,11 @@
 <script>
     import { onMount } from 'svelte';
-
+    /*
     onMount( ()=> {
         console.log('Platform not ready, re-directing to EHS');
         window.location.hash = '#ehs'; 
     })
+    */
 
 </script>
 
